7ced459bc49afd57302cbb3ba1eff59886c33f94
Tadeo <domenechtadeo@outlook.es>
2021-11-07 13:52:43 +0100

    fixed error netlify not compile

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import "./App.css";
-import CardRoobot from "./components/CardRoobot";
 
 function App() {
-  const roobot = {
-    features: { speed: 10, endurance: 2, creationDate: "1110" },
-    name: "C2P2",
-    imageSource: "url",
-  };
   return (
     <div className="App">
       <h1>Here they are all Roobots</h1>
-      <CardRoobot roobot={roobot} />
     </div>
   );
 }
