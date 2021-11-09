6ceb1f21a55722e8bf63d22c2833cb92617b143d
Tadeo <domenechtadeo@outlook.es>
2021-11-09 20:13:50 +0100

    Create postUserThunk

--- src/redux/thunks/roobotsThunks.js
+++ src/redux/thunks/roobotsThunks.js
@@ -1,8 +1,23 @@
 import axios from "axios";
-import { getRoobotsAction } from "../actions/actionCreators";
+import jwtDecode from "jwt-decode";
+import { getRoobotsAction, postUserAction } from "../actions/actionCreators";
+
+const apiUrl = process.env.REACT_APP_URL;
 
 export const getRoobotsThunks = () => async (dispatch) => {
-  const roobots = await axios.get(process.env.REACT_APP_URL);
+  const roobots = await axios.get(apiUrl);
 
   dispatch(getRoobotsAction(roobots.data));
+};
+
+export const postUserThunk = (user) => async (dispatch) => {
+  const response = await axios.post(`${apiUrl}user`, user);
+
+  if (response.status === 200) {
+    const token = response.data.token;
+    const user = jwtDecode(token);
+
+    dispatch(postUserAction(user));
+    localStorage.setItem("user", JSON.stringify({ token: token }));
+  }
 };
